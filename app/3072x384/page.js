c712d08d729820ab103a0f88ac0ca3165c979d4d
jaycheezy <jaycheezy@gmail.com>
2024-12-18 13:43:55 +0100

    third test page

---
+++ app/3072x384/page.js
@@ -0,0 +1,29 @@
+'use client'
+import { useState, useEffect } from 'react';
+
+export default function Page3072x384() {
+  const adUrls = [
+    "https://iframe.fresh8.co/index.html?id=675c0f8967f4ed0456136b35"
+  ];
+  
+  const [currentAdIndex, setCurrentAdIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentAdIndex(prev => (prev + 1) % adUrls.length);
+    }, 30000);
+    return () => clearInterval(interval);
+  }, [adUrls.length]);
+
+  return (
+    <div>
+      <iframe
+        key={currentAdIndex} // This key changes every 30s causing a re-mount
+        src={adUrls[currentAdIndex]}
+        width="3072"
+        height="384"
+        style={{ border: "none" }}
+      ></iframe>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -4,7 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      Some text
+      <p>Here are links to pages with ads</p>
+      <a href="/745x640">745x640</a>
+      <a href="/745x130">745x130</a>
+      <a href="/3072x384">3072x384</a>
     </div>
   );
 }
