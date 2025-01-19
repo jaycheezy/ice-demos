56f17a152a19d29914f4e4921f5c6ba4d2eb1cf1
jaycheezy <jaycheezy@gmail.com>
2025-01-19 16:36:27 +0100

    added QR Code to Leaderboard

--- app/745x640/page.js
+++ app/745x640/page.js
@@ -5,6 +5,7 @@ export default function Page745x640() {
   const items = [
     { src: "/pick.png", type: "image", duration: 10000 },
     { src: "https://iframe.fresh8.co/index.html?id=678656f801d3a51276650568", type: "iframe", duration: 30000 },
+    { src: "/pickQR.png", type: "image", duration: 15000 },
     { src: "https://iframe.fresh8.co/index.html?id=6789371901d3a5127665cbbf", type: "iframe", duration: 20000 },
     { src: "https://iframe.fresh8.co/index.html?id=6789052b01d3a51276657396", type: "iframe", duration: 20000 },
     { src: "https://iframe.fresh8.co/index.html?id=678a142201d3a5127665e29d", type: "iframe", duration: 20000 },
